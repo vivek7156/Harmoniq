9f83437ef532acae4fc39d6c0532d2598ac0409a
“Vivek <viveksingh7156@gmail.com>
2025-01-13 21:24:26 +0530

    project setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+
+function App() {
+  return (
+    <>
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+    </>
+  );
+}
+
+export default App;
